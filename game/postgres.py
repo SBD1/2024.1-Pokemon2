62ce3e7dfdf89d19dc6a3fa76ab24e54e9085092
Arthurlima544 <arthurlindaolima544@gmail.com>
2024-08-19 20:33:11 -0300

    pequenas alteracoes

--- game/postgres.py
+++ game/postgres.py
@@ -17,10 +17,12 @@ faker = Faker('en_US')
 for i in range(10):
     print(f"Inserting record number {i + 1}")
     Chance_Captura = faker.random_int(min=0, max=100)
-    Pokeball_TIPO = faker.random_int(min=1, max=10)
+    Pokeball_TIPO = faker.random_int(min=1, max=3)
     ID_Pokebola = faker.random_int(min=0, max=10000)
     insert = f"INSERT INTO Pokeball (ID_Pokebola,Chance_Captura,Pokeball_TIPO) VALUES ({ID_Pokebola},{Chance_Captura}, {Pokeball_TIPO})"
     db_engine.execute(text(insert).execution_options(autocommit=True))
+    get_pokeballs= "SELECT * FROM Pokeball;"
+    db_engine.execute(text(get_pokeballs).execution_options(autocommit=True))
     print(f"Complete record number {i + 1}")
 
 db_engine.close()
